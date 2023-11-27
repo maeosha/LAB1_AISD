3029efb9b1e3346dd221b5757fa17385cea585d4
maeosha <mochkauha123@gmail.com>
2023-11-27 12:26:36 +0400

    добавили вычислительные операторы

--- LAB1_AISD/Source.cpp
+++ LAB1_AISD/Source.cpp
@@ -77,3 +77,119 @@ Ratio* Polynomial<Ratio>::Get_container() const {
 
 	return container;
 }
+
+template <class Ratio>
+
+Ratio Polynomial<Ratio>::operator[](const size_t& index) const {
+
+	return container[index];
+}
+
+template <class Ratio>
+
+Polynomial<Ratio> Polynomial<Ratio>::operator+(Polynomial& summand) {
+	size_t min_size = this->size;
+	size_t max_size = summand.Get_size();
+	bool smallest = 0;
+	if (min_size > max_size) {
+		std::swap(min_size, max_size);
+		smallest = 1;
+	}
+	Polynomial value(max_size);
+	for (size_t index = 0; index < min_size; index++) {
+		value.Add_element(this->container[index] + summand[index], index);
+	}
+	for (size_t index = min_size; index < max_size; index++) {
+		if (smallest == 0) {
+			value.Add_element(summand[index], index);
+		}
+		else {
+			value.Add_element(this->container[index], index);
+		}
+	}
+	return value;
+}
+
+template <class Ratio>
+
+Polynomial<Ratio> Polynomial<Ratio>::operator*(const Ratio& multiplier) {
+	Polynomial value(this->size);
+	for (size_t index = 0; index < this->size; index++) {
+		value.Add_element(this->container[index] * multiplier, index);
+	}
+	return value;
+}
+
+template <class Ratio>
+
+Polynomial<Ratio> Polynomial<Ratio>::operator-(Polynomial& deductible) {
+	Polynomial value(1);
+	Polynomial negative_polynomial(1);
+	negative_polynomial = deductible * (-1);
+	value = *this + negative_polynomial;
+	return value;
+}
+
+
+template <class Ratio>
+
+void Polynomial<Ratio>::operator=(const Polynomial& _polynomial) {
+	delete[] container;
+	this->container = new Ratio[_polynomial.Get_size()];
+	for (size_t index = 0; index < _polynomial.Get_size(); index++) {
+		container[index] = _polynomial[index];
+	}
+	this->size = _polynomial.Get_size();
+}
+
+template <class Ratio>
+
+Ratio Polynomial<Ratio>::operator[](const size_t& index) const {
+
+	return container[index];
+}
+
+template <class Ratio>
+
+Polynomial<Ratio> Polynomial<Ratio>::operator+(Polynomial& summand) {
+	size_t min_size = this->size;
+	size_t max_size = summand.Get_size();
+	bool smallest = 0;
+	if (min_size > max_size) {
+		std::swap(min_size, max_size);
+		smallest = 1;
+	}
+	Polynomial value(max_size);
+	for (size_t index = 0; index < min_size; index++) {
+		value.Add_element(this->container[index] + summand[index], index);
+	}
+	for (size_t index = min_size; index < max_size; index++) {
+		if (smallest == 0) {
+			value.Add_element(summand[index], index);
+		}
+		else {
+			value.Add_element(this->container[index], index);
+		}
+	}
+	return value;
+}
+
+template <class Ratio>
+
+Polynomial<Ratio> Polynomial<Ratio>::operator*(const Ratio& multiplier) {
+	Polynomial value(this->size);
+	for (size_t index = 0; index < this->size; index++) {
+		value.Add_element(this->container[index] * multiplier, index);
+	}
+	return value;
+}
+
+template <class Ratio>
+
+Polynomial<Ratio> Polynomial<Ratio>::operator-(Polynomial& deductible) {
+	Polynomial value(1);
+	Polynomial negative_polynomial(1);
+	negative_polynomial = deductible * (-1);
+	value = *this + negative_polynomial;
+	return value;
+}
